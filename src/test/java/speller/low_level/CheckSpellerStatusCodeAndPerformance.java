917b0e8cec249e606ed791af1ecd4d2bdb345b91
VeronikaRevjakina <veronikaw777@gmail.com>
2022-02-09 22:17:18 +0300

    Update CheckSpellerStatusCodeAndPerformance.java

--- src/test/java/speller/low_level/CheckSpellerStatusCodeAndPerformance.java
+++ src/test/java/speller/low_level/CheckSpellerStatusCodeAndPerformance.java
@@ -26,7 +26,7 @@ public class CheckSpellerStatusCodeAndPerformance {
     private final String CORRECTFORM2 = "cucumbers";
     private final String CORRECTFORM3 = "cucomber";
 
-    private final String DOMAIN = getDomain();
+    private final String DOMAIN = "domain";
     private final String CHECKTEXT_ENDPOINT = DOMAIN +
             YandexSpellerConstants.CHECK_TEXT_URI;
 
